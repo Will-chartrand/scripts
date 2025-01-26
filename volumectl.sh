f634b936c7e6c844b1742e964dd114be1afc6351
will-chartrand <willchartrand@gmail.com>
2025-01-26 16:40:26 -0500

    add mute functinality for dwmblocks

--- volumectl.sh
+++ volumectl.sh
@@ -7,7 +7,19 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
+# Return
 currVolume=$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }' | tr -d %)
+
+if [ $1 = "-g" ]; then
+  muted=$(pactl get-sink-mute @DEFAULT_SINK@ | grep "yes" | wc -l)
+  if [ "$muted" == 1 ]; then
+    echo "---"
+    exit 1
+  fi
+  echo $(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')
+  exit 1
+fi
+
 if [ "$2" = "up" ]; then
   if [ "$currVolume" == 100 ]; then
     notify-send -t 1800 "MAX" -h string:x-canonical-private-synchronous:anything
@@ -33,7 +45,10 @@ if [ "$currVolume" == 0 ]; then
 elif [ "$currVolume" == 100 ]; then
   notify-send -t 1800 "MAX" -h string:x-canonical-private-synchronous:anything
 else
-  notify-send -t 1800 $(eval printf "%.0s\#" $(seq 1 $(echo "scale=0; $(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }' | tr -d %) / 4" | bc -l))) -h string:x-canonical-private-synchronous:anything
+  hashtags=$(eval printf "%.0s\#" $(seq 1 $(echo "scale=0; ($currVolume) / 5" | bc -l)))
+  dashes=$(eval printf "%.0s\-" $(seq 1 $(echo "scale=0; 20 - ($currVolume) / 5" | bc -l)))
+  volString="${hashtags}${dashes}"
+  notify-send -t 1800 $volString -h string:x-canonical-private-synchronous:anything
 fi
 
 exit 0
